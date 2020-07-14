b40fa94e19d395c6df08e9d7cf46ae58f9c78a36
Mark V. Senofsky <mark.v.senofsky@gmail.com>
2020-07-14 08:46:43 -0400

    Improve comments in sway scripts
    For improved code comprehensibility:
    - Improve comments in get-cwd-of-focused-window.sh
    - Improve comments in get-name-of-monitor-containing-focused-window.sh

--- sway/scripts/get-cwd-of-focused-window.sh
+++ sway/scripts/get-cwd-of-focused-window.sh
@@ -1,7 +1,13 @@
-# This scripts prints the current working directory of the focused window
-
 #!/usr/bin/env bash
+#
+# This scripts prints the current working directory of the focused window
+#
 
-pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
-ppid=$(pgrep --newest --parent ${pid})
-readlink /proc/${ppid}/cwd || echo $HOME
+# Get the PID of the focused window (which is a terminal)
+focused_window_pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
+
+# Get the PID of the focused window's newest child process (which is a shell)
+shell_pid=$(pgrep --newest --parent $focused_window_pid)
+
+# Reads (which prints) the symbolic link to the shell's CWD
+readlink /proc/$shell_pid/cwd || echo $HOME
--- sway/scripts/get-name-of-monitor-containing-focused-window.sh
+++ sway/scripts/get-name-of-monitor-containing-focused-window.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
+#
 # Prints the name of the monitor that contains the focused window
-
-#! /usr/bin/env bash
+#
 
 swaymsg -r -t get_outputs | jq -j '.[] | select(.focused == true) | .name'
